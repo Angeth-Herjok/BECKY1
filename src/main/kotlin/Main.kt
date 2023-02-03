be970ca2cecdf4f4039d4b8fdfdfa0eb13e14c66
Angeth-Herjok <angethherjok@gmail.com>
2023-02-03 08:33:43 +0300

    james

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+
+    var name = "james"
+    var  age = "20"
+    var weight = "68kg"
+    var phonenumber = "0798764523"
+    var citizenship = "kenyan"
+    println(age)
+    println(weight)
+    println(name)
+
+}
